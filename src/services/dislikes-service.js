ee85152784912ff6fa68b7ccd391abf7bfc32996
SpencerSolomon <spencersolomon6@gmail.com>
2022-03-28 19:36:19 -0400

    added documentation to the dislikes service file to describe the microservices it provides

--- src/services/dislikes-service.js
+++ src/services/dislikes-service.js
@@ -7,10 +7,21 @@ const api = axios.create({
     withCredentials: true
 });
 
+/**
+ * 
+ * @param {string} uid the user id of the user to toggle the dislike button for 
+ * @param {string} tid the tuit id of the tuit to be disliked
+ * @returns the tuit object after the like toggle has been performed
+ */
 export const userTogglesTuitDislikes = (uid, tid) =>
     api.put(`${USERS_API}/${uid}/dislikes/${tid}`)
         .then(response => response.data);
 
+/**
+ * 
+ * @param {string} userId the user id of the user to find disliked tuits for 
+ * @returns an array of tuits disliked by the given user
+ */
 export const findAllTuitsDislikedByUser = (userId) =>
     api.get(`${USERS_API}/${userId}/dislikes`)
         .then(response => response.data);
